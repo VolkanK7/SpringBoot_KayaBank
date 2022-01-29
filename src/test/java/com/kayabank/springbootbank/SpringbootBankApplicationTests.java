7472f66f635206a3bfc7a27a4d489393c1ac7d57
VolkanK7 <volkan.kaya007@gmail.com>
2022-01-29 14:01:59 +0300

    Initial commit

---
+++ src/test/java/com/kayabank/springbootbank/SpringbootBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayabank.springbootbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
