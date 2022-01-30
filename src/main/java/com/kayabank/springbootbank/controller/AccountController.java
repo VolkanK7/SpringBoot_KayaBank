680cdbbe6e3c6a668af8b3aabbd91b9a8c2b446f
VolkanK7 <volkan.kaya007@gmail.com>
2022-01-30 21:10:52 +0300

    Account CRUD Operations Done

--- src/main/java/com/kayabank/springbootbank/controller/AccountController.java
+++ src/main/java/com/kayabank/springbootbank/controller/AccountController.java
@@ -2,12 +2,13 @@ package com.kayabank.springbootbank.controller;
 
 import com.kayabank.springbootbank.dto.AccountDto;
 import com.kayabank.springbootbank.dto.CreateAccountRequest;
+import com.kayabank.springbootbank.dto.CustomerDto;
+import com.kayabank.springbootbank.dto.UpdateAccountRequest;
 import com.kayabank.springbootbank.service.AccountService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/account")
@@ -22,5 +23,27 @@ public class AccountController {
     @PostMapping("/create")
     public ResponseEntity<Object> createAccount(@RequestBody CreateAccountRequest createAccountRequest){
         return ResponseEntity.ok(accountService.createAccount(createAccountRequest));
+    }
+
+    @GetMapping("/getall")
+    public ResponseEntity<List<AccountDto>> getAllAccounts(){
+        return ResponseEntity.ok(accountService.getAllAccounts());
+    }
+
+    @GetMapping("/getaccount/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable String id){
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<AccountDto> updateAccount(@PathVariable String id,
+                                                    @RequestBody UpdateAccountRequest updateAccountRequest){
+        return ResponseEntity.ok(accountService.updateAccount(id, updateAccountRequest));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable String id){
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok("Account Deleted!");
     }
 }
--- src/main/java/com/kayabank/springbootbank/service/AccountService.java
+++ src/main/java/com/kayabank/springbootbank/service/AccountService.java
@@ -3,11 +3,16 @@ package com.kayabank.springbootbank.service;
 import com.kayabank.springbootbank.dto.AccountDto;
 import com.kayabank.springbootbank.dto.AccountDtoConverter;
 import com.kayabank.springbootbank.dto.CreateAccountRequest;
+import com.kayabank.springbootbank.dto.UpdateAccountRequest;
 import com.kayabank.springbootbank.exception.CustomerNotFoundException;
 import com.kayabank.springbootbank.model.Account;
 import com.kayabank.springbootbank.model.Customer;
 import com.kayabank.springbootbank.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountService {
@@ -35,5 +40,41 @@ public class AccountService {
                 .city(createAccountRequest.getCity())
                 .build();
         return accountDtoConverter.convert(accountRepository.save(account));
+    }
+
+    public AccountDto updateAccount(String id, UpdateAccountRequest request){
+        Customer customer = customerService.getCustomerById(request.getCustomerId());
+        if(customer.getId() == null || customer.getId().trim().equals("")){
+            throw new CustomerNotFoundException("Customer Not Found!");
+        }
+
+        Optional<Account> accountOptional = accountRepository.findById(id);
+        accountOptional.ifPresent(account -> {
+            account.setBalance(request.getBalance());
+            account.setCity(request.getCity());
+            account.setCurrency(request.getCurrency());
+            account.setCustomerId(request.getCustomerId());
+            accountRepository.save(account);
+        });
+        return accountOptional.map(accountDtoConverter::convert)
+                .orElseThrow(() -> new CustomerNotFoundException("Customer Not Found!"));
+    }
+
+    public List<AccountDto> getAllAccounts(){
+       List<Account> accountList = accountRepository.findAll();
+
+       return accountList.stream()
+               .map(accountDtoConverter :: convert)
+               .collect(Collectors.toList());
+    }
+
+    public AccountDto getAccountById(String id){
+        return accountRepository.findById(id)
+                .map(accountDtoConverter::convert)
+                .orElseThrow(() -> new CustomerNotFoundException("Customer Not Found!"));
+    }
+
+    public void deleteAccount(String id) {
+        accountRepository.deleteById(id);
     }
 }
