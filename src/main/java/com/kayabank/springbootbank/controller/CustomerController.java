1dc72cdafecf30673d0f82ec1dc3afad264c211f
VolkanK7 <volkan.kaya007@gmail.com>
2022-01-30 14:55:30 +0300

    Get All Customers Operation

--- src/main/java/com/kayabank/springbootbank/controller/CustomerController.java
+++ src/main/java/com/kayabank/springbootbank/controller/CustomerController.java
@@ -6,6 +6,8 @@ import com.kayabank.springbootbank.service.CustomerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/customer")
 public class CustomerController {
@@ -19,5 +21,10 @@ public class CustomerController {
     @PostMapping("/create")
     public ResponseEntity<CustomerDto> createCustomer(@RequestBody CreateCustomerRequest createCustomerRequest){
         return ResponseEntity.ok(customerService.createCustomer(createCustomerRequest));
+    }
+
+    @GetMapping("/getall")
+    public ResponseEntity<List<CustomerDto>> getAllCustomers(){
+        return ResponseEntity.ok(customerService.getAllCustomers());
     }
 }
--- src/main/java/com/kayabank/springbootbank/service/CustomerService.java
+++ src/main/java/com/kayabank/springbootbank/service/CustomerService.java
@@ -1,19 +1,24 @@
 package com.kayabank.springbootbank.service;
 
-import com.kayabank.springbootbank.dto.CityDto;
 import com.kayabank.springbootbank.dto.CreateCustomerRequest;
 import com.kayabank.springbootbank.dto.CustomerDto;
+import com.kayabank.springbootbank.dto.CustomerDtoConverter;
 import com.kayabank.springbootbank.model.City;
 import com.kayabank.springbootbank.model.Customer;
 import com.kayabank.springbootbank.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter customerDtoConverter;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter customerDtoConverter) {
         this.customerRepository = customerRepository;
+        this.customerDtoConverter = customerDtoConverter;
     }
 
     public CustomerDto createCustomer(CreateCustomerRequest createCustomerRequest){
@@ -25,13 +30,16 @@ public class CustomerService {
         customer.setCity(City.valueOf(createCustomerRequest.getCity().name()));
 
         customerRepository.save(customer);
-        CustomerDto customerDto = new CustomerDto();
-        customerDto.setId(customer.getId());
-        customerDto.setAddress(customer.getAddress());
-        customerDto.setDateOfBirth(customer.getDateOfBirth());
-        customerDto.setName(customer.getName());
-        customerDto.setCity(CityDto.valueOf(customer.getCity().name()));
 
-        return customerDto;
+        return customerDtoConverter.convert(customer);
+    }
+
+    public List<CustomerDto> getAllCustomers() {
+        List<Customer> customerList = customerRepository.findAll();
+        List<CustomerDto> customerDtoList = new ArrayList<>();
+        for(Customer customer : customerList){
+            customerDtoList.add(customerDtoConverter.convert(customer));
+        }
+        return customerDtoList;
     }
 }
