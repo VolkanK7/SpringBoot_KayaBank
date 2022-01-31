35ed02d7a70e28e42fb5fa4bb19e8c21e5e20538
VolkanK7 <volkan.kaya007@gmail.com>
2022-01-31 22:13:53 +0300

    Account DTOs

--- src/main/java/com/kayabank/springbootbank/dto/AccountDto.java
+++ src/main/java/com/kayabank/springbootbank/dto/AccountDto.java
@@ -9,6 +9,8 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
+@EqualsAndHashCode
 public class AccountDto{
     private String id;
     private String customerId;
--- src/main/java/com/kayabank/springbootbank/dto/CreateAccountRequest.java
+++ src/main/java/com/kayabank/springbootbank/dto/CreateAccountRequest.java
@@ -1,14 +1,12 @@
 package com.kayabank.springbootbank.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CreateAccountRequest extends BaseAccountRequest{
 
     private String id;
