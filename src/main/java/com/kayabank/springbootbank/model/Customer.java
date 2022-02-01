59e280ffec0941bfeb315872a9d2c658e1d04c26
VolkanK7 <volkan.kaya007@gmail.com>
2022-02-01 14:17:12 +0300

    Customer Validations done

--- src/main/java/com/kayabank/springbootbank/model/Customer.java
+++ src/main/java/com/kayabank/springbootbank/model/Customer.java
@@ -3,6 +3,9 @@ package com.kayabank.springbootbank.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -15,12 +18,17 @@ import javax.persistence.*;
 @EqualsAndHashCode
 public class Customer {
 
+    @Pattern(regexp = "^[1-9]{1}[0-9]{9}[02468]{1}$")
     @Id
     private String id;
-
+    @NotBlank(message = "Name must be not null")
+    @Size(min = 3, message = "Name must be min 3 of characters")
     private String name;
+    @NotBlank(message = "Date of birth must be not blank")
     @Column(name = "date_of_birth")
     private Integer dateOfBirth;
+    @NotBlank(message = "Date of birth must be not blank")
     private City city;
+    @NotBlank(message = "Date of birth must be not blank")
     private String address;
 }
