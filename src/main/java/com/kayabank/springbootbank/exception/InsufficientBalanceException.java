671b11cea860dc0ed0d9c497c7b197f3dca0a959
VolkanK7 <volkan.kaya007@gmail.com>
2022-02-02 13:15:05 +0300

    Exception Handling

--- src/main/java/com/kayabank/springbootbank/exception/InsufficientBalanceException.java
+++ src/main/java/com/kayabank/springbootbank/exception/InsufficientBalanceException.java
@@ -3,7 +3,7 @@ package com.kayabank.springbootbank.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InsufficientBalanceException extends RuntimeException{
     public InsufficientBalanceException(String message) {
         super(message);
